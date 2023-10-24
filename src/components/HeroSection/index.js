013565984e6798b43adbb3acac6e0e6eed2dcb02
Jmenzies722 <Jmenzies722@gmail.com>
2023-10-24 12:29:54 -0400

    fav icon

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,7 +28,7 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Download Resume</ResumeButton>
+                        <ResumeButton href={Bio.resume} target='display'>Resume</ResumeButton>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
