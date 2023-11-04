3e861190094242a4e920ee5acec6cf868e67f6f9
Jmenzies722 <Jmenzies722@gmail.com>
2023-11-04 15:46:38 -0400

    updated

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -6,7 +6,7 @@ export const HeroContainer = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
-  padding: 170px 30px;
+  padding: 175px 30px;
   @media (max-width: 960px) {
     padding: 30px 16px;
   }
@@ -96,20 +96,18 @@ export const HeroRightContainer = styled.div`
 export const Img = styled.img`
   position: relative;
   width: 100%;
-  height: 100%;
-  max-width: 400px;
-  max-height: 400px;
+  height: auto;
+  max-width: 500px;
+  max-height: 500px;
   border-radius: 50%;
-  border: 2px solid ${({ theme }) => theme.primary};
+  border: 3px solid ${({ theme }) => theme.primary};
 
   @media (max-width: 768px) {
     max-width: 400px;
-    max-height: 400px;
   }
 
   @media (max-width: 640px) {
     max-width: 280px;
-    max-height: 280px;
   }
 `;
 
